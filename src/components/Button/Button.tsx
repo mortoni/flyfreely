d56ff617e8e7ce7971625aa26ea2b06abd4861db
Alan Mortoni <mortoni.alan@hotmail.com>
2019-05-24 20:42:48 +1000

    first commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import StyledButton from './styled/StyledButton';
+
+export interface ButtonProps {
+  /** Callback function to be called when user clicks on button */
+  onClick: () => void;
+  children: React.ReactNode;
+  /** Renders a disabled button and prevents onClick */
+  disabled?: boolean;
+}
+
+const Button: React.SFC<ButtonProps> = ({
+  children,
+  disabled = false,
+  onClick,
+}) => (
+  <StyledButton disabled={disabled} onClick={onClick}>
+    {children}
+  </StyledButton>
+);
+
+export default Button;
