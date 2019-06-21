92800daa041fb0a1126a8f32c2429011b6e057a2
Alan Mortoni <mortoni.alan@hotmail.com>
2019-06-21 21:54:10 +1000

    fix: improvements

--- src/components/Dashboard/Dashboard.stories.tsx
+++ src/components/Dashboard/Dashboard.stories.tsx
@@ -8,6 +8,13 @@ import Dashboard from './Dashboard'
 storiesOf('Dashboard', module)
     .addDecorator(jsxDecorator)
     .addDecorator(withKnobs)
+    .addParameters({
+        backgrounds: [
+            { name: 'grey', value: '#ccc', default: true },
+            { name: 'none', value: '#fff' },
+            { name: 'black', value: '#000' },
+        ],
+    })
     .add('normal', () => (
         <Dashboard />
     ));
