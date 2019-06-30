13e9f9c47337e5cb3fa9e8c8ee7fd80b985199ff
Alan Mortoni <mortoni.alan@hotmail.com>
2019-06-30 19:42:23 +1000

    comment out menu stories

--- src/components/Menu/menu.stories.tsx
+++ src/components/Menu/menu.stories.tsx
@@ -1,31 +1,128 @@
-import { object } from '@storybook/addon-knobs'
-import { storiesOf } from '@storybook/react'
-import * as React from 'react'
-import Menu from './Menu'
+// import Battery20Icon from '@material-ui/icons/Battery20'
+// import BatteryStdIcon from '@material-ui/icons/BatteryStd'
+// import BugReportIcon from '@material-ui/icons/BugReport'
+// import BuildIcon from '@material-ui/icons/Build'
+// import FlightIcon from '@material-ui/icons/Flight'
+// import LibraryBooksIcon from '@material-ui/icons/LibraryBooks'
+// import LinkIcon from '@material-ui/icons/Link'
+// import ListIcon from '@material-ui/icons/List'
+// import ListAltIcon from '@material-ui/icons/ListAlt'
+// import LocationOnIcon from '@material-ui/icons/LocationOn'
+// import PeopleIcon from '@material-ui/icons/People'
+// import WarningIcon from '@material-ui/icons/Warning'
+// import { action, object } from '@storybook/addon-knobs'
+// import { storiesOf } from '@storybook/react'
+// import * as React from 'react'
+// import Menu from './Menu'
 
-const menu = object('menu', [
-    { label: 'Missions', icon: '', route: '', selected: true },
-    { label: 'Personnel', icon: '', route: '', selected: true },
-    { label: 'Locations', icon: '', route: '', selected: true },
-    { label: 'Approvals', icon: '', route: '', selected: true },
-    { label: 'RPA', icon: '', route: '', selected: true },
-    { label: 'Maintenance', icon: '', route: '', selected: true },
-    {
-        icon: '',
-        label: 'Flight Battery Sets',
-        route: '',
-        selected: true,
-    },
-    { label: 'Batteries', icon: '', route: '', selected: true },
-    { label: 'Labraries', icon: '', route: '', selected: true },
-    { label: 'Reporting', icon: '', route: '', selected: true },
-    {
-        icon: '',
-        label: 'Incident Reports',
-        route: '',
-        selected: true,
-    },
-    { label: 'Risk Register', icon: '', route: '', selected: true },
-])
+// const menu = object('menu', {
+//     dialogAction: () => action('open-dialog'),
+//     component: null,
+//     icon: ListIcon,
+//     key: 'mission',
+//     label: 'Missions',
+//     selected: true,
+//     size: {},
+// },
+// {
+//     dialogAction: () => action('open-dialog'),
+//     component: null,
+//     icon: FlightIcon,
+//     key: 'rpa',
+//     label: 'RPA',
+//     selected: true,
+//     size: { md: 6 },
+// },
+// {
+//     dialogAction: () => action('open-dialog'),
+//     component: null,
+//     icon: PeopleIcon,
+//     key: 'personnel',
+//     label: 'Personnel',
+//     selected: true,
+//     size: { md: 6 },
+// },
+// {
+//     dialogAction: () => action('open-dialog'),
+//     component: null,
+//     icon: Battery20Icon,
+//     key: 'batteries',
+//     label: 'Batteries',
+//     selected: true,
+//     size: {},
+// },
+// {
+//     dialogAction: () => action('open-dialog'),
+//     component: null,
+//     icon: BatteryStdIcon,
+//     key: 'batterySets',
+//     label: 'Flight Battery Sets',
+//     selected: true,
+//     size: {},
+// },
+// {
+//     dialogAction: () => action('open-dialog'),
+//     component: null,
+//     icon: ListAltIcon,
+//     key: 'approvals',
+//     label: 'Approvals',
+//     selected: true,
+//     size: {},
+// },
+// {
+//     dialogAction: () => action('open-dialog'),
+//     component: null,
+//     icon: BuildIcon,
+//     key: 'maintenance',
+//     label: 'Maintenance',
+//     selected: true,
+//     size: {},
+// },
+// {
+//     dialogAction: () => action('open-dialog'),
+//     component: null,
+//     icon: LocationOnIcon,
+//     key: 'locations',
+//     label: 'Locations',
+//     selected: true,
+//     size: { md: 6 },
+// },
+// {
+//     dialogAction: () => action('open-dialog'),
+//     component: null,
+//     icon: LibraryBooksIcon,
+//     key: 'libraries',
+//     label: 'Libraries',
+//     selected: true,
+//     size: { md: 6 },
+// },
+// {
+//     dialogAction: () => action('open-dialog'),
+//     component: null,
+//     icon: BugReportIcon,
+//     key: 'incident',
+//     label: 'Incident Reports',
+//     selected: true,
+//     size: {},
+// },
+// {
+//     dialogAction: () => action('open-dialog'),
+//     component: null,
+//     icon: WarningIcon,
+//     key: 'risk',
+//     label: 'Risk Register',
+//     selected: true,
+//     size: {},
+// },
+// {
+//     component: null,
+//     dialogAction: () => action('open-dialog'),
+//     icon: LinkIcon,
+//     key: 'organisationLinks',
+//     label: 'Organisation Links',
+//     selected: true,
+//     size: { md: 6 },
+// }
+// ])
 
-storiesOf('Components/Menu', module).add('normal', () => <Menu menu={menu} />)
+// storiesOf('Components/Menu', module).add('normal', () => <Menu menu={menu} setMenu={action('set-menu')}/>)
