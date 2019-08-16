573b507dfd06afb70c8997110dd5bc90f46b6bd8
Alan Mortoni <mortoni.alan@hotmail.com>
2019-08-16 16:16:50 +1000

    added adon for viewport

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -4,7 +4,7 @@ import { createMuiTheme } from '@material-ui/core/styles'
 import { MuiPickersUtilsProvider } from '@material-ui/pickers'
 import ThemeProvider from '@material-ui/styles/ThemeProvider'
 import { select, withKnobs } from '@storybook/addon-knobs/react'
-import { addDecorator, configure } from '@storybook/react'
+import { addDecorator, addParameters, configure } from '@storybook/react'
 import * as React from 'react'
 import { jsxDecorator } from 'storybook-addon-jsx'
 import { ThemeProvider as StyledProvider } from 'styled-components'
@@ -27,6 +27,39 @@ addDecorator((story) => {
 })
 addDecorator(withKnobs)
 addDecorator(jsxDecorator)
+
+const newViewports = {
+    galaxyS8: {
+        name: 'Samsung Galaxy S8+',
+        styles: {
+            height: '740px',
+            width: '360px',
+        },
+    },
+    ipadPro: {
+        name: 'iPad Pro 12.9-inch',
+        styles: {
+            height: '1366px',
+            width: '1024px',
+        },
+    },
+    iphone6: {
+        name: 'iPhone 6',
+        styles: {
+            height: '667px',
+            width: '375px',
+        },
+    },
+    kindleFireHD: {
+        name: 'Kindle Fire HD',
+        styles: {
+            height: '801px',
+            width: '533px',
+        },
+    },
+}
+
+addParameters({ viewport: { viewports: newViewports } })
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /.stories.tsx$/)
---
+++ src/components/Header/Header.stories.tsx
@@ -0,0 +1,5 @@
+import { storiesOf } from '@storybook/react'
+import * as React from 'react'
+import Header from './Header'
+
+storiesOf('components/Header', module).add('default', () => <Header />)
